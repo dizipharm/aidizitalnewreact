61467b3c86691e2d3c41445e552b8f766b147e00
pmsushil <pmsushil.2011@gmail.com>
2023-05-03 17:22:33 +0530

    New changes

--- src/components/Aboutus/New3.jsx
+++ src/components/Aboutus/New3.jsx
@@ -11,12 +11,12 @@ const New3 = () => {
   return (
   <>
   <div className=' w-full pt-8 px-4'>
-  <div className=' flex gap-8  bg-black text-white justify-center '>
+  <div className=' flex flex-col md:flex-row md:gap-8  bg-black text-white justify-center '>
   <div className=' flex   p-12 text-3xl text-center'>
-    <h1>Explore With Us <span className=' text-[#17b1b1]'> The Platform</span>  </h1>
+    <h1>Explore With Us <span className=' text-[#17b1b1] '> The Platform</span>  </h1>
  
    </div>
-  <div className=' flex flex-col justify-center'>
+  <div className=' items-center p-4 px-12 md:pt-12'>
   
   
             <Link to='/contact'>
